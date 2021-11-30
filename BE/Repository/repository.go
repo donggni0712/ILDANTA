08a513d92d54046512050bf72a8ea9a6e0862a99
donggni0712 <donggni0712@naver.com>
2021-11-30 14:01:09 +0000

    :art:Make Click Function

--- BE/Repository/repository.go
+++ BE/Repository/repository.go
@@ -5,7 +5,7 @@ import (
 	"../ApiClient"
 	"../Domain"
 )
-func ShowFirstRoute(SX string, SY string, EX string, EY string, apikey string) {
+func ShowFirstRoute(SX string, SY string, EX string, EY string, apikey string) []*Domain.Result {
 	var results []*Domain.Result
 	
 	results = ApiClient.CallRoute(SX,SY,EX,EY,apikey)
@@ -17,4 +17,33 @@ func ShowFirstRoute(SX string, SY string, EX string, EY string, apikey string) {
 			fmt.Printf("\t%10s %10s %10s\n\t\t\t",firstPath.Name,firstPath.TransferNum,firstPath.TotalTime)
 		}
 	}
+		
+	return results
+}
+
+func ShowFirstPath(firstPath *Domain.FirstPath){
+	fmt.Printf("일단 %15s에서 %10s 탐\n\n",firstPath.Where,firstPath.Name)
+	fmt.Printf("\t내려서\t\t여기서\t\t다시 타")
+	for _,afterPathTheme := range firstPath.AfterPathThemes{
+		fmt.Printf("\n==========================================================\n%10s\t",afterPathTheme.Getoff)
+		for _,afterPath := range afterPathTheme.AfterPaths{
+			fmt.Printf("\t%10s\t%10s\n\t\t\t",afterPath.Getin,afterPath.NextName)
+		}
+	}
+
+}
+
+func ClickRoute(where string, what string,results []*Domain.Result){
+	
+	for _,result := range results{
+		if result.Where == where{
+			for _,firstPath := range result.FirstPaths{
+				if firstPath.Name == what{
+					ShowFirstPath(firstPath)
+					return
+				}
+			}
+		}
+	}
+	fmt.Println("검색값 없음")
 }
--- BE/main.go
+++ BE/main.go
@@ -2,6 +2,8 @@ package main
  
 import (
 	"./Repository"
+	"./Domain"
+	"fmt"
 )
 
 func main() {
@@ -10,5 +12,16 @@ func main() {
 	EX := "127.05981200975921"
 	EY := "37.28556112210226"
 	apikey := "&apiKey=Mi%2B95EDTMwWb2pbwhatNbhwx4tE4XkBsZ1GiAS2HoGI"
-	Repository.ShowFirstRoute(SX,SY,EX,EY,apikey)
+	
+	var res []*Domain.Result
+	
+	  res = Repository.ShowFirstRoute(SX,SY,EX,EY,apikey)
+	
+	var where string
+	var what string
+	fmt.Println()
+	fmt.Scanf("%s",&where)
+	fmt.Scanf("%s",&what)
+	
+	Repository.ClickRoute(where,what,res)
 }
